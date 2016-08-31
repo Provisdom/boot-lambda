2174ebfca8aea571241c2fd55c3adb21d0b66a8d
Kenny Williams <kennyjwilli@gmail.com>
2016-08-31 13:45:10 -0700

    Formatting!

--- src/provisdom/boot_lambda.clj
+++ src/provisdom/boot_lambda.clj
@@ -27,34 +27,34 @@
                             :local-file nil)))
 
 (core/deftask create-function
-              [f function-name VAL str "The name you want to assign to the function you are uploading"
-               r runtime VAL str "The runtime environment for the Lambda function you are uploading"
-               a region VAL str "AWS region"
-               i role VAL str "The ARN of the IAM role that Lambda assumes when it executes your function"
-               p handler VAL sym "The function within your code that Lambda calls to begin execution"
-               d description VAL str "A short, user-defined function description"
-               m memory-size VAL int "The amount of memory, in MB, your Lambda function is given"
-               t timeout VAL int "The function execution time at which Lambda should terminate the function"
-               l local-file VAL str "The path to the local file of the code you are uploading"]
-              (when-not (and function-name runtime role handler)
-                (throw (Exception. "Required function-name, runtime, role, and handler to create function")))
-              (core/with-pre-wrap fileset
-                                  (let [input-files (core/input-files fileset)
-                                        cmd-string (->> *opts* (task-opts->cmd-opts input-files) (cmd-opts->string "aws lambda create-function"))]
-                                    (util/info "Creating lambda function...\n")
-                                    (util/info (str cmd-string "\n"))
-                                    (shell cmd-string))
-                                  fileset))
+  [f function-name VAL str "The name you want to assign to the function you are uploading"
+   r runtime VAL str "The runtime environment for the Lambda function you are uploading"
+   a region VAL str "AWS region"
+   i role VAL str "The ARN of the IAM role that Lambda assumes when it executes your function"
+   p handler VAL sym "The function within your code that Lambda calls to begin execution"
+   d description VAL str "A short, user-defined function description"
+   m memory-size VAL int "The amount of memory, in MB, your Lambda function is given"
+   t timeout VAL int "The function execution time at which Lambda should terminate the function"
+   l local-file VAL str "The path to the local file of the code you are uploading"]
+  (when-not (and function-name runtime role handler)
+    (throw (Exception. "Required function-name, runtime, role, and handler to create function")))
+  (core/with-pre-wrap fileset
+    (let [input-files (core/input-files fileset)
+          cmd-string (->> *opts* (task-opts->cmd-opts input-files) (cmd-opts->string "aws lambda create-function"))]
+      (util/info "Creating lambda function...\n")
+      (util/info (str cmd-string "\n"))
+      (shell cmd-string))
+    fileset))
 
 (core/deftask update-function
-              [f function-name VAL str "The name you want to assign to the function you are uploading"
-               l local-file VAL str "The path to the local file of the code you are uploading"]
-              (when-not function-name
-                (throw (Exception. "Required function-name to update function")))
-              (core/with-pre-wrap fileset
-                                  (let [input-files (core/input-files fileset)
-                                        cmd-string (->> *opts* (task-opts->cmd-opts input-files) (cmd-opts->string "aws lambda update-function-code"))]
-                                    (util/info "Updating lambda function...\n")
-                                    (util/info (str cmd-string "\n"))
-                                    (shell cmd-string))
-                                  fileset))
+  [f function-name VAL str "The name you want to assign to the function you are uploading"
+   l local-file VAL str "The path to the local file of the code you are uploading"]
+  (when-not function-name
+    (throw (Exception. "Required function-name to update function")))
+  (core/with-pre-wrap fileset
+    (let [input-files (core/input-files fileset)
+          cmd-string (->> *opts* (task-opts->cmd-opts input-files) (cmd-opts->string "aws lambda update-function-code"))]
+      (util/info "Updating lambda function...\n")
+      (util/info (str cmd-string "\n"))
+      (shell cmd-string))
+    fileset))
